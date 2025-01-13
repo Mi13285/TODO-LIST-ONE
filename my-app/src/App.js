f4e35bf7ba77a5bfaf21755653be9e593402bea5
Mi13285 <mixkud95@inbox.ru>
2025-01-13 12:12:38 +0300

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import Todo from "./Todo.js";
+import TodoForm from "./TodoForm.js";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTask = (userInput) => {
+    if (userInput) {
+      const newItem = {
+        id: Math.random().toString(36).substring(2, 9),
+        task: userInput,
+        complete: false,
+      };
+      setTodos([...todos, newItem]);
+    }
+  };
+  const removeTask = () => {};
+  const handleToggle = () => {};
+  return (
+    <div className="App">
+      <heder>
+        <h1>Список задач :{todos.length}</h1>
+      </heder>
+      <TodoForm addTask={addTask} />
+      {todos.map((todo) => {
+        return (
+          <Todo
+            todo={todo}
+            key={todo.id}
+            toggle={handleToggle}
+            removeTask={removeTask}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
