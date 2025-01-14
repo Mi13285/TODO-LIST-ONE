8428030f64eca7a55513bcdb629c609dac07eac9
Mi13285 <mixkud95@inbox.ru>
2025-01-14 08:05:17 +0300

    informashion about project

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,8 +4,10 @@ import Todo from "./Todo.js";
 import TodoForm from "./TodoForm.js";
 function App() {
   const [todos, setTodos] = useState([]);
+  console.log(todos);
 
   const addTask = (userInput) => {
+    console.log(userInput);
     if (userInput) {
       const newItem = {
         id: Math.random().toString(36).substring(2, 9),
@@ -13,6 +15,7 @@ function App() {
         complete: false,
       };
       setTodos([...todos, newItem]);
+      console.log(setTodos);
     }
   };
   const removeTask = (id) => {
